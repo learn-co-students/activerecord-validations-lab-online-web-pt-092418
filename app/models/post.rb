614e19c4f3b4b14948a3b61674665d7b68399a7b
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-03-07 12:46:01 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    puts "got here with #{post.title}"
+    if !post.title
+      return false
+    elsif post.title.downcase.include? "won't believe"
+      return true
+    elsif post.title.downcase.include? "secret"
+      return true
+    elsif post.title.downcase.include? "top [number]"
+      return true
+    elsif post.title.downcase.include? "guess"
+      return true 
+    else 
+      post.errors[:name] = "No clickbait"
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  validates :title, presence: true
+  validates :content, length: {minimum: 250 }
+  validates :summary, length: {maximum: 250} 
+  validates :category, :inclusion => { :in => ["Fiction", "Non-Fiction"] }
 end
